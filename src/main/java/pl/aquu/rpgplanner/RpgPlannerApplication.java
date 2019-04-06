d2902c3c18e2fad2727283f2d2d7eefa0886ae23
Marcin Lesiński <marcin.lesinski.87@gmail.com>
2019-04-06 09:18:37 +0200

    Project scheme.

---
+++ src/main/java/pl/aquu/rpgplanner/RpgPlannerApplication.java
@@ -0,0 +1,15 @@
+package pl.aquu.rpgplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgPlannerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RpgPlannerApplication.class, args);
+    }
+
+}
